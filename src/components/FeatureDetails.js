730cc15c0ff050cfcdf9f9c5bca999d7533ebf7f
Omri Gazitt <ogazitt@gmail.com>
2021-10-07 10:22:30 -0700

    more fixes to front-page code

--- src/components/FeatureDetails.js
+++ src/components/FeatureDetails.js
@@ -8,10 +8,11 @@ const FeatureDetailsList = [
     Svg: require('../../static/img/Section-1.svg').default,
     description: (
       <pre className={styles.featureDetailsCode}>
-        $ policy build . -t myorg/peoplefinder:1.0.0<br/>
-        $ policy tag myorg/peoplefinder:1.0.0 myorg/peoplefinder<br/>
-        $ policy push myorg/peoplefinder<br/>
-        $ policy pull myorg/peoplefinder<br/>
+        {`$ policy build . -t myorg/peoplefinder:1.0.0
+$ policy tag myorg/peoplefinder:1.0.0 myorg/peoplefinder
+$ policy push myorg/peoplefinder
+$ policy pull myorg/peoplefinder
+`}
       </pre>
     ),
   },
@@ -20,10 +21,10 @@ const FeatureDetailsList = [
     Svg: require('../../static/img/Section-2.svg').default,
     description: (
       <pre className={styles.featureDetailsCode}>
-        $ cosign init<br/>
-        $ cosign generate-key-pair<br/>
-        $ cosign sign -key cosign.key myorg/peoplefinder:1.0.0<br/>
-        $ cosign verify -key cosign.pub myorg/peoplefinder:1.0.0<br/>
+        {`$ cosign init
+$ cosign generate-key-pair
+$ cosign sign -key cosign.key myorg/peoplefinder:1.0.0
+$ cosign verify -key cosign.pub myorg/peoplefinder:1.0.0`}
       </pre>
     ),
   },
@@ -32,9 +33,9 @@ const FeatureDetailsList = [
     Svg: require('../../static/img/Section-3.svg').default,
     description: (
       <pre className={styles.featureDetailsCode}>
-        $ policy repl myorg/peoplefinder:1.0.0<br/>
-        {`> data.system.bundles`}<br/>
-        {`{
+        {`$ policy repl myorg/peoplefinder:1.0.0
+> data.system.bundles
+{
   "/Users/ogazitt/.policy/policies-root/blobs/sha256/84d...7e9": {
     "manifest": {
       "revision": "",
