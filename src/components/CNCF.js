659a6e77beaca1d8362bb8a66f180b6335b646c6
Omri Gazitt <ogazitt@gmail.com>
2023-01-31 11:34:38 -0800

    added LF trademark statement

--- src/components/CNCF.js
+++ src/components/CNCF.js
@@ -13,6 +13,8 @@ const CNCF = () =>
       <center>
         <h2>We are a Cloud Native Computing Foundation sandbox project.</h2>
         <img width="500px" src="https://raw.githubusercontent.com/cncf/artwork/master/other/cncf/horizontal/white/cncf-white.svg" alt="CNCF logo" />
+        <br />
+        <p>The Linux Foundation has registered trademarks and uses trademarks. For a list of trademarks of The Linux Foundation, please see our <a href="https://www.linuxfoundation.org/trademark-usage" target="_">Trademark Usage</a> page.</p>
       </center>
     </div>
   </section>
